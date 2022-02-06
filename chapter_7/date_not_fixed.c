a943cc368ab10eb29b5a65517863bf7ca5ba6183
imryche <imryche13@gmail.com>
2022-02-06 18:12:51 +0300

    add not fixed dates example

---
+++ chapter_7/date_not_fixed.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int day, month, year;
+  char monthname[20];
+
+  char *line;
+  size_t linesize = 100;
+
+  line = (char *)malloc(linesize * sizeof(char));
+  if (line == NULL) {
+    printf("error: unable to allocate buffer");
+    exit(1);
+  }
+
+  while (getline(&line, &linesize, stdin) > 0) {
+    if (sscanf(line, "%d %s %d", &day, monthname, &year) == 3)
+      printf("valid: %s\n", line);
+    else if (sscanf(line, "%d/%d/%d", &month, &day, &year) == 3)
+      printf("valid: %s\n", line);
+    else
+      printf("invalid: %s\n", line);
+  }
+
+  return 0;
+}
